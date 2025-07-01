9b844e9fff99c39a0106ab089bf3c2ab8fba6780
William Gong <williamg8936@gmail.com>
2025-06-30 21:53:19 -0700

    Set up config and environment variables

--- app.config.ts
+++ app.config.ts
@@ -1,9 +1,36 @@
 import { ExpoConfig, ConfigContext } from '@expo/config';
 
+const getBundleId = () => {
+	const appVariant = process.env.APP_VARIANT;
+
+	switch (appVariant) {
+		case 'production':
+			return 'com.mwang8936.bfc';
+		case 'preview':
+			return 'com.mwang8936.bfc.preview';
+		default:
+			return 'com.mwang8936.bfc.dev';
+	}
+};
+
+const bundleID = getBundleId();
+
+const getAppName = () => {
+	const appVariant = process.env.APP_VARIANT;
+	switch (appVariant) {
+		case 'production':
+			return 'BFC';
+		case 'preview':
+			return 'BFC (Preview)';
+		default:
+			return 'BFC (Dev)';
+	}
+};
+
 export default ({ config }: ConfigContext): ExpoConfig => {
 	return {
 		...config,
-		name: 'BFC',
+		name: getAppName(),
 		slug: 'BFC',
 		newArchEnabled: true,
 		version: '1.0.0',
@@ -17,9 +44,14 @@ export default ({ config }: ConfigContext): ExpoConfig => {
 			backgroundColor: '#fff200',
 		},
 		ios: {
+			bundleIdentifier: bundleID,
 			supportsTablet: true,
+			config: {
+				usesNonExemptEncryption: false,
+			},
 		},
 		android: {
+			package: bundleID,
 			adaptiveIcon: {
 				foregroundImage: './assets/images/adaptive-icon.png',
 				backgroundColor: '#ffffff',
